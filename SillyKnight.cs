9517aecb81c3653fdd3ffaf867343f9c5df96b40
itsjohncs <johnsullivan.pem@gmail.com>
2021-03-16 13:30:34 -0700

    WIP: trying to track spawned go.
    I've figured out the fundamental components. Tryna figure out how I
    want to structure the implementation now.

--- SillyKnight.cs
+++ SillyKnight.cs
@@ -36,13 +36,93 @@ namespace SillyKnight {
         public override void Initialize() {
             base.Initialize();
 
-            Modding.ModHooks.Instance.OnEnableEnemyHook += HandleEnemyEnabled;
-
             // This is called _immediately_ after the game objects in the scene
-            // are instantiated. Which means we can get their exact spawn
-            // positions!
+            // are instantiated. Perfect time to grab their spawn positions for
+            // their nametags.
             UnityEngine.SceneManagement.SceneManager.sceneLoaded +=
                 HandleSceneChanged;
+
+            Modding.ModHooks.Instance.OnEnableEnemyHook += HandleEnemyEnabled;
+
+            // Die is in the callstack of OnEnable when geo is flung from
+            // corpses. So we can store the dying enemy in Die, and then grab
+            // it in OnEnable, to associate Geo with a particular enemy.
+            On.HealthManager.Die += HandleEnemyDeath;
+            On.GeoControl.OnEnable += HandleGeoSpawned;
+
+            On.GeoControl.Getter += GeoControl_Getter;
+            On.GeoControl.Disable += GeoControl_Disable;
+            On.GeoControl.PlayCollectSound += GeoControl_PlayCollectSound;
+            On.HeroController.AddGeo += HeroController_AddGeo;
+            On.HeroController.AddGeoQuietly += HeroController_AddGeoQuietly;
+        }
+
+        private void HeroController_AddGeoQuietly(On.HeroController.orig_AddGeoQuietly orig, HeroController self, int amount)
+        {
+            Log("Stack trace in HeroController_AddGeoQuietly:");
+            Log(IndentString(System.Environment.StackTrace));
+
+            orig(self, amount);
+        }
+
+        private void HeroController_AddGeo(On.HeroController.orig_AddGeo orig, HeroController self, int amount)
+        {
+            Log("Stack trace in HeroController_AddGeo:");
+            Log(IndentString(System.Environment.StackTrace));
+
+            orig(self, amount);
+        }
+
+        private float GeoControl_PlayCollectSound(On.GeoControl.orig_PlayCollectSound orig, GeoControl self)
+        {
+            Log("Stack trace in GeoControl_PlayCollectSound:");
+            Log(IndentString(System.Environment.StackTrace));
+
+            return orig(self);
+        }
+
+        private void GeoControl_Disable(On.GeoControl.orig_Disable orig, GeoControl self, float waitTime)
+        {
+            Log("Stack trace in GeoControl_Disable:");
+            Log(IndentString(System.Environment.StackTrace));
+
+            orig(self, waitTime);
+        }
+
+        private IEnumerator GeoControl_Getter(On.GeoControl.orig_Getter orig, GeoControl self)
+        {
+            Log("Stack trace in GeoControl_Getter:");
+            Log(IndentString(System.Environment.StackTrace));
+
+            return orig(self);
+        }
+
+        private void HandleGeoSpawned(On.GeoControl.orig_OnEnable orig, GeoControl self)
+        {
+            try {
+                CollectibleKey spawnedBy = KeyBox.GetKey();
+                if (self.gameObject.GetComponent<SpawnedByHolder>() == null) {
+                    SpawnedByHolder link =
+                        self.gameObject.AddComponent<SpawnedByHolder>();
+                    link.SpawnedBy = spawnedBy;
+                }
+            } catch (System.Exception e) {
+                LogException("Error in HandleGeoSpawned", e);
+            }
+        }
+
+        private void HandleEnemyDeath(On.HealthManager.orig_Die orig, HealthManager self, float? attackDirection, AttackTypes attackType, bool ignoreEvasion)
+        {
+            try {
+                KeyBox box = new KeyBox(GetKeyFromNametag(self.gameObject));
+                try {
+                    orig(self, attackDirection, attackType, ignoreEvasion);
+                } finally {
+                    box.Dispose();
+                }
+            } catch (System.Exception e) {
+                LogException("Error in HandleEnemyDeath", e);
+            }
         }
 
         private void HandleSceneChanged(UnityEngine.SceneManagement.Scene _, UnityEngine.SceneManagement.LoadSceneMode _1)
@@ -50,14 +130,16 @@ namespace SillyKnight {
             try {
                 foreach (GameObject gameObject in
                          UnityEngine.Object.FindObjectsOfType<GameObject>()) {
-                    MaybeAttachKey(gameObject);
+                    // Nametags will be attached to lots of non-enemy objects,
+                    // but they won't do any harm.
+                    MaybeAttachNametag(gameObject);
                 }
             } catch (System.Exception e) {
                 LogException("Error in HandleSceneChanged", e);
             }
         }
 
-        private void MaybeAttachKey(GameObject gameObject) {
+        private void MaybeAttachNametag(GameObject gameObject) {
             if (gameObject.GetComponent<Nametag>() == null) {
                 Nametag nametag = gameObject.AddComponent<Nametag>();
 
@@ -68,17 +150,7 @@ namespace SillyKnight {
             }
         }
 
-        private GameObject HandleObjectCreated(GameObject gameObject) {
-            try {
-                AttachKey(gameObject);
-            } catch (System.Exception e) {
-                LogException("Error in HandleObjectCreated", e);
-            }
-
-            return gameObject;
-        }
-
-        private CollectibleKey GetKey(GameObject gameObject) {
+        private CollectibleKey GetKeyFromNametag(GameObject gameObject) {
             Nametag nametag = gameObject.GetComponent<Nametag>();
             if (nametag != null) {
                 return nametag.Key;
@@ -91,17 +163,12 @@ namespace SillyKnight {
             try {
                 HealthManager healthManager =
                     gameObject.GetComponent<HealthManager>();
-                if (isDead || healthManager == null) {
+                if (healthManager == null) {
                     return isDead;
                 }
 
-                CollectibleKey key = GetKey(gameObject);
-                CollectibleState? state = Collectibles.TryGet(key);
-                if (state == null || state == CollectibleState.Uncollected) {
-                    healthManager.OnDeath += () => Collectibles.TrySet(
-                        key,
-                        CollectibleState.Collected);
-                } else if (state == CollectibleState.Collected) {
+                CollectibleKey key = GetKeyFromNametag(gameObject);
+                if (Collectibles.TryGet(key) == CollectibleState.Collected) {
                     healthManager.SetGeoLarge(0);
                     healthManager.SetGeoMedium(0);
                     healthManager.SetGeoSmall(0);
---
+++ SpawnedByHolder.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+
+namespace SillyKnight {
+    class SpawnedByHolder : MonoBehaviour {
+        public CollectibleKey SpawnedBy;
+    }
+}
